62a733e662f28c32e009114fe5d54856ffbc7711
William Rieger <wrieger93@gmail.com>
2017-07-31 22:52:16 -0700

    Add Clone impl for BiHashMap

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,15 @@ struct BiHashMap<L, R> {
     right2left: HashMap<Rc<R>, Rc<L>>,
 }
 
+impl<L, R> Clone for BiHashMap<L, R> where L: Clone, R: Clone {
+    fn clone(&self) -> BiHashMap<L, R> {
+        BiHashMap {
+            left2right: self.left2right.clone(),
+            right2left: self.right2left.clone(),
+        }
+    }
+}
+
 impl<L, R> BiHashMap<L, R> {}
 
 #[cfg(test)]
