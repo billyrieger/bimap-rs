59ac4f04bc9eb3fcc6b1557c5f133e39c16b3363
William Rieger <wrieger93@gmail.com>
2017-08-01 01:06:46 -0700

    Add some functions for BiHashMap Specifically, new(), len(), is_empty(), get_by_left(), get_by_right(), contains_left(), and contains_right().

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 use std::hash::Hash;
+use std::ops::Deref;
 use std::rc::Rc;
 
 /// A bijective map backed by `std::collections::HashMap`.
-struct BiHashMap<L, R> {
+pub struct BiHashMap<L, R> {
     left2right: HashMap<Rc<L>, Rc<R>>,
     right2left: HashMap<Rc<R>, Rc<L>>,
 }
@@ -51,7 +52,46 @@ where
     }
 }
 
-impl<L, R> BiHashMap<L, R> {}
+impl<L, R> BiHashMap<L, R> 
+where
+    L: Eq + Hash,
+    R: Eq + Hash,
+{
+    /// Creates an empty `BiHashMap`.
+    pub fn new() -> BiHashMap<L, R> {
+        BiHashMap::default()
+    }
+
+    /// Returns the number of left-right pairs in the map.
+    pub fn len(&self) -> usize {
+        self.left2right.len()
+    }
+
+    /// Returns `true` if the map contains no elements, and `false` otherwise.
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Returns a reference to the right value corresponding to the left key.
+    pub fn get_by_left(&self, left: &L) -> Option<&R> {
+        self.left2right.get(left).map(Deref::deref)
+    }
+
+    /// Returns a reference to the left value corresponding to the right key.
+    pub fn get_by_right(&self, right: &R) -> Option<&L> {
+        self.right2left.get(right).map(Deref::deref)
+    }
+
+    /// Returns `true` if the map contains the specified left element, and `false` otherwise.
+    pub fn contains_left(&self, left: &L) -> bool {
+        self.left2right.contains_key(left)
+    }
+
+    /// Returns `true` if the map contains the specified left element, and `false` otherwise.
+    pub fn contains_right(&self, right: &R) -> bool {
+        self.right2left.contains_key(right)
+    }
+}
 
 #[cfg(test)]
 mod tests {
