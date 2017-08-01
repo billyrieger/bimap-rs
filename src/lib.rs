369fd7f6a83cac5d293c1339d21d21b2e38cf919
William Rieger <wrieger93@gmail.com>
2017-07-31 22:49:23 -0700

    Add BiHashMap struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
+use std::collections::HashMap;
+use std::rc::Rc;
+
+/// A bijective map backed by `std::collections::HashMap`.
+struct BiHashMap<L, R> {
+    left2right: HashMap<Rc<L>, Rc<R>>,
+    right2left: HashMap<Rc<R>, Rc<L>>,
+}
+
+impl<L, R> BiHashMap<L, R> {}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }
